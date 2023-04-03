c54db69513f1b1baaab57349a7361147b6616b9e
Gabrielparizet <parizetgabriel@gmail.com>
2023-04-03 14:56:20 +0200

    (feat) Tempo detector post route working in sending response to client

--- api/app.py
+++ api/app.py
@@ -3,12 +3,13 @@ from flask import Flask, request
 # Instantiate flask app and assign it to app variable.
 app = Flask(__name__)
 
-# @app.get("/")
-# def test_route():
-#    return "Your connected to your flask server"
+from sound_detector import find_tempo
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
     audio_file = request.files["audioFile"]
     audio_file.save("/Users/gabrielparizet/Desktop/Sound_Data/api/audio_files/" + audio_file.filename)
-    return "File uploaded successfully"
+    new_path = "/Users/gabrielparizet/Desktop/Sound_Data/api/audio_files/" + audio_file.filename
+    find_tempo(new_path)
+    return find_tempo(new_path)
+
