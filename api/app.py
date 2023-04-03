494981b9fb4be197e6090f520fe17ed8ea118267
Gabrielparizet <parizetgabriel@gmail.com>
2023-04-03 15:32:23 +0200

    (feat) Add audio_file length to upload post route reponse in app.py

--- api/app.py
+++ api/app.py
@@ -3,13 +3,18 @@ from flask import Flask, request
 # Instantiate flask app and assign it to app variable.
 app = Flask(__name__)
 
-from sound_detector import find_tempo
+# Importing find_tempo method from tempo_detector
+from tempo_detector import find_tempo
+
+# Importing find_length method from length_detector
+from length_detector import find_length
+
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
-    audio_file = request.files["audioFile"]
-    audio_file.save("/Users/gabrielparizet/Desktop/Sound_Data/api/audio_files/" + audio_file.filename)
-    new_path = "/Users/gabrielparizet/Desktop/Sound_Data/api/audio_files/" + audio_file.filename
-    find_tempo(new_path)
-    return find_tempo(new_path)
+   audio_file = request.files["audioFile"]
+   audio_file.save("/Users/gabrielparizet/Desktop/Sound_Data/api/audio_files/" + audio_file.filename)
+   new_path = "/Users/gabrielparizet/Desktop/Sound_Data/api/audio_files/" + audio_file.filename
+   result_response = find_tempo(new_path) + " " + find_length(new_path)
+   return result_response
 
---
+++ api/length_detector.py
@@ -0,0 +1,20 @@
+import librosa
+
+# find_length takes an audio file as argument and returns a formatted string that indicates the length of that audio file.
+def find_length(file_name):
+    # librosa.load is used to load the audio file into memory as a waveform "y" and a sample rate "sr".
+    y, sr = librosa.load(file_name)
+    # Get the duration of our audiofile in seconds with librosa get_duration method.
+    duration = librosa.get_duration(y=y, sr=sr)
+    file_duration = seconds_to_minutes(duration)
+    return f'Your audio file is {file_duration} long.'
+
+
+# The seconds_to_minutes function takes time in float as an argument and converts it to string giving its time in minutes and seconds. 
+def seconds_to_minutes(time):
+    formated = "{:.2f}".format(time/60)
+    formated = str(formated)
+    x = formated.split('.')
+    minutes = x[0]
+    seconds = x[1]
+    return f'{minutes} minutes and {seconds} seconds'
